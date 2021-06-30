08fcad5c43ba7296ec0eda326347ed1022fc3545
Settapong Supkong <settapong.s@kbtg.tech>
2021-06-30 12:27:17 +0700

    first commit

---
+++ src/main/java/com/example/potterbook/PotterBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.potterbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotterBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotterBookApplication.class, args);
+	}
+
+}
