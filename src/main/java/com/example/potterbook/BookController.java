142f68ca0d1c270f2808bc9eddfe59d7e577b563
Settapong Supkong <settapong.s@kbtg.tech>
2021-06-30 14:49:34 +0700

    count two book different

--- src/main/java/com/example/potterbook/BookController.java
+++ src/main/java/com/example/potterbook/BookController.java
@@ -5,9 +5,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BookController {
 
+//    public static void main(String[] args) {
+//        CountTwoDiff(new int[]{1,1,1,1,0});
+//    }
+
     public float getPrice(int book1,int book2,int book3,int book4,int book5){
         float price = 0;
+        int[] books = new int[]{book1,book2,book3,book4,book5};
         price = (book1 + book2 + book3 + book4 + book5) * 100;
+        int twoDiff = CountTwoDiff(books);
         return price;
+    }
+
+    public int CountTwoDiff(int[] books) {
+        int twoDiff = 0;
+        int count = 0;
+        int[] temp = new int[]{0,0,0,0,0};
+        while (count==0){
+            for (int i = 0; i < books.length; i++) {
+                if (books[i] > 0 && count < 2){
+                    count++;
+                    temp[i] = 1;
+                }
+            }
+            if (count < 2) {
+                break;
+            }else {
+                twoDiff++;
+                count = 0;
+                for (int i = 0; i < books.length; i++) {
+                    books[i] -= temp[i];
+                    temp[i] = 0;
+//                    System.out.print(books[i]+ " ;");
+                }
+            }
+        }
+
+        return twoDiff;
     }
 }
--- src/test/java/com/example/potterbook/BookControllerTest.java
+++ src/test/java/com/example/potterbook/BookControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.potterbook;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,24 @@ public class BookControllerTest {
     public void GetPriceWithNoPromotion() {
         BookController bookController = new BookController();
         assertEquals(300,bookController.getPrice(3,0,0,0,0));
+    }
+
+    @Test
+    public void CountTwoDiff() {
+        BookController bookController = new BookController();
+        assertEquals(1,bookController.CountTwoDiff(new int[]{0,1,0,1,0}));
+    }
+
+    @Test
+    public void CountTwoDiff02() {
+        BookController bookController = new BookController();
+        assertEquals(2,bookController.CountTwoDiff(new int[]{1,2,1,0,0}));
+    }
+
+    @Test
+    @DisplayName("2 diff books -5%")
+    public void GetPriceWithPromotion01() {
+        BookController bookController = new BookController();
+        assertEquals(190,bookController.getPrice(0,0,1,1,0));
     }
 }
